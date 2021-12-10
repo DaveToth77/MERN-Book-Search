a75da81022b9af426c0f306fa78293954f570499
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-12-10 08:04:57 -0500

    added GET_ME query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,7 +2,22 @@ import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
 
-
+  query getMe {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        _id
+        title
+        authors
+        description
+        image
+        link
+      }
+    }
+  }
 `
 
 
