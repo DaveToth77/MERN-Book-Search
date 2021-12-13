b1a6c85697b0ab6627651aaaa7a22fb62e5904fb
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-12-13 09:48:39 -0500

    removed duplicate data from merge...

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,15 +1,6 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  type Query {
-    me: User
-  }
-  type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(input: bookInput): User
-    removeBook(bookId: ID!): User
-  }
   type User {
     _id: ID
     username: String
