05a7c267d892ee8aea2e2006774ac3e39f295516
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-12-17 09:07:47 -0500

    changed port

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:3000/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
